e621a4ad87584385f8abed56ce2590c3123cbeff
DEV ADMIN <admin@domation.vn>
2026-07-06 14:33:28 +0700

    Add version tag v1.0.1 to db_auto_optimize.php

--- api/db_auto_optimize.php
+++ api/db_auto_optimize.php
@@ -43,7 +43,7 @@ function printLog($message, $type = 'info') {
     }
 }
 
-printLog("=== KHỞI ĐỘNG HỆ THỐNG BẢO TRÌ DATABASE AUTOFLOW ===", 'info');
+printLog("=== KHỞI ĐỘNG HỆ THỐNG BẢO TRÌ DATABASE AUTOFLOW v1.0.1 ===", 'info');
 
 // 1. Connection
 $pdo = null;
