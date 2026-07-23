cdfc93d864d38bb7fb933dccef87a1191131e478
DEV ADMIN <admin@domation.vn>
2026-07-23 15:42:09 +0700

    chore: add run_workers_internal.php

---
+++ api/run_workers_internal.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/db_connect.php';
+require_once __DIR__ . '/worker_flow.php';
+
+$GLOBALS['current_admin_id'] = 'admin-001';
+$res = runWorkerFlow($pdo);
+header('Content-Type: application/json');
+echo json_encode($res, JSON_PRETTY_PRINT);
